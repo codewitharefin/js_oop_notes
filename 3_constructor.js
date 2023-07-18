d665ed0809c309e6e3f9c85d40bd17708fffcc73
Md. Morshedul Arefin <arefin2k@gmail.com>
2023-07-18 17:05:58 +0600

    initial

---
+++ 3_constructor.js
@@ -0,0 +1,29 @@
+// ✅ ✅ ✅ Constructor
+
+// class Player {
+//     constructor(name,age,skill) {
+//         this.name = name;
+//         this.age = age;
+//         this.skill = skill;
+//     }
+//     sayHello() {
+//         return `Hi, I am ${this.name} - ${this.skill}`;
+//     }
+// }
+
+// const mashrafi = new Player("Mashrafi", 35, "Bowler");
+// const sakib = new Player("Mashrafi", 34, "All-rounder");
+// const mushfiq = new Player("Mashrafi", 32, "Wicket-keeper");
+
+// console.log(mashrafi.age);
+// console.log(mashrafi.name);
+// console.log(mashrafi.skill);
+
+// console.log(sakib.sayHello());
+
+// Output: 
+// 35
+// Mashrafi
+// Bowler
+// Hi, I am Mashrafi - All-rounder
+
---
+++ 5_inheritance.js
@@ -0,0 +1,40 @@
+
+// ✅ ✅ ✅ Inheritance
+
+// class Human {
+//     constructor(name,age) {
+//         this.name = name;
+//         this.age = age;
+//         this.arms = 2;
+//         this.legs = 2;
+//     }
+// }
+
+// class Baby extends Human {
+//     constructor(name,age) {
+//         super(name,age);
+//         this.cute = true;
+//     }
+//     cry() {
+//         return `Waa waa!`;
+//     }
+// }
+
+// class Teenager extends Human {
+//     constructor(name,age) {
+//         super(name,age);
+//         this.emotional = true;
+//     }
+//     crush() {
+//         return `I have a crush on ***`;
+//     }
+// }
+
+// const babu = new Baby("Asaduzzaman Babu", 5);
+// console.log(babu.name);
+// console.log(babu.age);
+// Output:
+// Asaduzzaman Babu
+// 5
+
+
